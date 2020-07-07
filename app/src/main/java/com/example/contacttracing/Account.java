992313353cdac81f9d5ecbeb8ecc06e4e326e346
mckymse <hemhemanth411819.hk@gmail.com>
2020-07-08 00:18:28 +0530

    profile

--- app/src/main/java/com/example/contacttracing/Account.java
+++ app/src/main/java/com/example/contacttracing/Account.java
@@ -1,64 +1,176 @@
 package com.example.contacttracing;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Account#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+import java.util.concurrent.Executor;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
+
 public class Account extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CircleImageView circleImageView;
+    public Uri imageuri;
+    private FirebaseStorage firebaseStorage;
+    LinearLayout button;
+    private StorageReference storageReference;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firestore;
+    TextView tv1,tv2,tv3;
+    String uid;
 
-    public Account() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Account.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Account newInstance(String param1, String param2) {
-        Account fragment = new Account();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button=view.findViewById(R.id.logout);
+        circleImageView = view.findViewById(R.id.pic);
+        tv1=view.findViewById(R.id.name);
+        tv2=view.findViewById(R.id.mail);
+        tv3=view.findViewById(R.id.phone);
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference= firebaseStorage.getReference();
+        firebaseAuth=FirebaseAuth.getInstance();
+        firestore=FirebaseFirestore.getInstance();
+        uid=firebaseAuth.getCurrentUser().getUid();
+        Log.d(TAG, "onViewCreated: "+uid);
+        final DocumentReference documentReference=firestore.collection("Users").document(uid);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    Log.d(TAG, "Current data: " + documentSnapshot.getData());
+                    tv1.setText(documentSnapshot.getString("Name"));
+                    tv2.setText(documentSnapshot.getString("email"));
+                    tv3.setText(documentSnapshot.getString("Phone"));
+                } else {
+                    Log.d(TAG, "Current data: null");
+                }
+
+            }
+        });
+
+
+        StorageReference storageReference1 = storageReference.child("users/"+firebaseAuth.getCurrentUser().getUid()+"/images.jpg");
+        storageReference1.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(circleImageView);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(),Signin.class));
+                getActivity().finish();
+
+            }
+        });
+        circleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture();
+            }
+
+
+        });
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageuri = data.getData();
+            circleImageView.setImageURI(imageuri);
+            uploadPicture(imageuri);
+        }
+        
+    }
+
+    private void uploadPicture(Uri imageuri) {
+
+
+        final StorageReference riversRef = storageReference.child("users/"+firebaseAuth.getCurrentUser().getUid()+"/images.jpg");
+
+        riversRef.putFile(imageuri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Toast.makeText(getActivity(),"Image Uploaded",Toast.LENGTH_SHORT).show();
+                        riversRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Picasso.get().load(uri).into(circleImageView);
+                            }
+                        });
+
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                        Toast.makeText(getActivity(),"Failed to upload",Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
