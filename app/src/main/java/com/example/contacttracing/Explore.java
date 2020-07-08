574154b9738898a26f9e4b7c62419c83b1b667f4
Naveen <naveen.jaisankar1999@gmail.com>
2020-07-08 15:19:30 +0530

    Explore Page Added without image

--- app/src/main/java/com/example/contacttracing/Explore.java
+++ app/src/main/java/com/example/contacttracing/Explore.java
@@ -1,19 +1,38 @@
 package com.example.contacttracing;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.contacttracing.Adapter.MyItemGroupAdapter;
+import com.example.contacttracing.Interface.IFirebaseLoadListener;
+import com.example.contacttracing.Model.ItemData;
+import com.example.contacttracing.Model.ItemGroup;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Explore#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Explore extends Fragment {
+public class Explore extends Fragment implements IFirebaseLoadListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -46,6 +65,12 @@ public class Explore extends Fragment {
         return fragment;
     }
 
+    AlertDialog dailog;
+    IFirebaseLoadListener iFirebaseLoadListener;
+
+    RecyclerView my_recycler_view;
+    DatabaseReference myData;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,12 +78,60 @@ public class Explore extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        myData = FirebaseDatabase.getInstance().getReference();
+        iFirebaseLoadListener= this;
+
+//        my_recycler_view = (RecyclerView)this.getView().findViewById(R.id.my_recyclr_view);
+//        my_recycler_view.setHasFixedSize(true);
+//        my_recycler_view.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        getFirebaseData();
+    }
+
+    private void getFirebaseData() {
+
+        myData.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<ItemGroup> itemGroups = new ArrayList<>();
+                for(DataSnapshot groupSnapShot : snapshot.getChildren()){
+                    ItemGroup itemGroup = new ItemGroup();
+                    itemGroup.setHeaderTitle(groupSnapShot.child("headerTitle").getValue(true).toString());
+                    GenericTypeIndicator<ArrayList<ItemData>> genericTypeIndicator = new GenericTypeIndicator<ArrayList<ItemData>>() {
+                    };
+                    itemGroup.setListitem(groupSnapShot.child("listitem").getValue(genericTypeIndicator));
+                    itemGroups.add(itemGroup);
+                }
+                iFirebaseLoadListener.onFirebaseLoadSuccess(itemGroups);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                iFirebaseLoadListener.onFirebaseLoadFailed(error.getMessage());
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        View RootView = inflater.inflate(R.layout.fragment_explore, container, false);
+        my_recycler_view = (RecyclerView)RootView.findViewById(R.id.my_recyclr_view);
+        my_recycler_view.setHasFixedSize(true);
+        my_recycler_view.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return RootView;
+    }
+
+    @Override
+    public void onFirebaseLoadSuccess(List<ItemGroup> itemGroupList) {
+        MyItemGroupAdapter adapter = new MyItemGroupAdapter(getActivity(), itemGroupList);
+        my_recycler_view.setAdapter(adapter);
+    }
+
+    @Override
+    public void onFirebaseLoadFailed(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
     }
 }
